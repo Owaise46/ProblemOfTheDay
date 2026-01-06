4fc0cc94cab8217e155f0e77edc646782bb8d21f
Owaise46 <137081719+Owaise46@users.noreply.github.com>
2026-01-06 14:59:16 +0530

    Max Xor Subarray of size K

---
+++ Gfg/MaxXorSubarrayofsizeK.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int maxSubarrayXOR(int[] arr, int k) {
+        int n = arr.length;
+        int xor = 0;
+
+        // XOR of first window
+        for (int i = 0; i < k; i++) {
+            xor ^= arr[i];
+        }
+
+        int maxXor = xor;
+
+        // Sliding window
+        for (int i = k; i < n; i++) {
+            xor ^= arr[i - k]; // remove left element
+            xor ^= arr[i];     // add right element
+            maxXor = Math.max(maxXor, xor);
+        }
+
+        return maxXor;
+    }
+}
